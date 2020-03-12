9530a7258943eadf6613291e50b21566db31ef21
Alejandra I. Lee Vargas <leevargasale@gmail.com>
2020-03-12 23:38:01 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -13,5 +13,8 @@ end
 
 def age_appropriate_birthday
   birthday_kids.each do |kids_name, age|
-    puts "Happy Birthday #{kids_name}! You are now #{age} years old!" if age <13
+    if age < 13
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!" 
+    end
+  end
 end
